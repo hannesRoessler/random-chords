65819c75f3b692c40bc80927071ba52556b8cec8
hannesRoessler <hannes.roessler@gmx.de>
2019-01-03 08:32:20 +0100

    update

--- app.js
+++ app.js
@@ -4,102 +4,158 @@ let audio = new Audio("blip.wav");
 let tempo = 0;
 // TODO: convert to JSON file and import
 var chordObj = [
-    { name: "C", checked: true, mainCat: "chordKey" },
-    { name: "C#", checked: true, mainCat: "chordKey" },
-    { name: "Db", checked: true, mainCat: "chordKey" },
-    { name: "D", checked: true, mainCat: "chordKey" },
-    { name: "D#", checked: true, mainCat: "chordKey" },
-    { name: "Eb", checked: true, mainCat: "chordKey" },
-    { name: "E", checked: true, mainCat: "chordKey" },
-    { name: "E#", checked: false, mainCat: "chordKey" },
-    { name: "Fb", checked: false, mainCat: "chordKey" },
-    { name: "F", checked: true, mainCat: "chordKey" },
-    { name: "F#", checked: true, mainCat: "chordKey" },
-    { name: "Gb", checked: true, mainCat: "chordKey" },
-    { name: "G", checked: true, mainCat: "chordKey" },
-    { name: "G#", checked: true, mainCat: "chordKey" },
-    { name: "Ab", checked: true, mainCat: "chordKey" },
-    { name: "A", checked: true, mainCat: "chordKey" },
-    { name: "A#", checked: false, mainCat: "chordKey" },
-    { name: "Bb", checked: true, mainCat: "chordKey" },
-    { name: "B", checked: true, mainCat: "chordKey" },
-    { name: "H", checked: false, mainCat: "chordKey" },
-    { name: "Cb", checked: false, mainCat: "chordKey" },
-    { name: "maj<sup>7</sup>", checked: true, mainCat: "chordType", subCat: "Major" },
-    { name: "△<sup>7</sup>", checked: true, mainCat: "chordType", subCat: "Major" },
-    { name: "MA<sup>7</sup>", checked: true, mainCat: "chordType", subCat: "Major" },
-    { name: "-<sup>7</sup>", checked: true, mainCat: "chordType", subCat: "Minor"},
-    { name: "min<sup>7</sup>", checked: true, mainCat: "chordType", subCat: "Minor"},
-    { name: "m<sup>7b5</sup>", checked: true, mainCat: "chordType", subCat: "Half-dimished" },
-    { name: "<sup>⍉</sup>", checked: true, mainCat: "chordType", subCat: "Half-dimished" },
-    { name: "<sup>7</sup>", checked: true, mainCat: "chordType", subCat: "Half-dimished" },
-    { name: "sus<sup>4</sup>", checked: false, mainCat: "chordType", subCat: "Suspended" },
-    // TODO: define subcategories and structure content better
+  { name: "C", checked: true, mainCat: "chordKey" },
+  { name: "C#", checked: true, mainCat: "chordKey" },
+  { name: "Db", checked: true, mainCat: "chordKey" },
+  { name: "D", checked: true, mainCat: "chordKey" },
+  { name: "D#", checked: true, mainCat: "chordKey" },
+  { name: "Eb", checked: true, mainCat: "chordKey" },
+  { name: "E", checked: true, mainCat: "chordKey" },
+  { name: "E#", checked: false, mainCat: "chordKey" },
+  { name: "Fb", checked: false, mainCat: "chordKey" },
+  { name: "F", checked: true, mainCat: "chordKey" },
+  { name: "F#", checked: true, mainCat: "chordKey" },
+  { name: "Gb", checked: true, mainCat: "chordKey" },
+  { name: "G", checked: true, mainCat: "chordKey" },
+  { name: "G#", checked: true, mainCat: "chordKey" },
+  { name: "Ab", checked: true, mainCat: "chordKey" },
+  { name: "A", checked: true, mainCat: "chordKey" },
+  { name: "A#", checked: false, mainCat: "chordKey" },
+  { name: "Bb", checked: true, mainCat: "chordKey" },
+  { name: "B", checked: true, mainCat: "chordKey" },
+  { name: "H", checked: false, mainCat: "chordKey" },
+  { name: "Cb", checked: false, mainCat: "chordKey" },
+  {
+    name: "maj<sup>7</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Major"
+  },
+  {
+    name: "△<sup>7</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Major"
+  },
+  {
+    name: "MA<sup>7</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Major"
+  },
+  {
+    name: "-<sup>7</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Minor"
+  },
+  {
+    name: "min<sup>7</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Minor"
+  },
+  {
+    name: "m<sup>7b5</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Half-dimished"
+  },
+  {
+    name: "<sup>⍉</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Half-dimished"
+  },
+  {
+    name: "<sup>7</sup>",
+    checked: true,
+    mainCat: "chordType",
+    subCat: "Half-dimished"
+  },
+  {
+    name: "sus<sup>4</sup>",
+    checked: false,
+    mainCat: "chordType",
+    subCat: "Suspended"
+  }
+  // TODO: define subcategories and structure content better
 ];
 const chordName = chordObj.name;
 console.log(chordName);
-console.log(chordObj)
+console.log(chordObj);
 function showChordSymbol() {
-    var meter = document.getElementById("meter-input").value;
-    var chordKey = chordObj.filter(chordObj => {
-        return chordObj.checked === true && chordObj.mainCat == "chordKey";
-    })
-    var chordType = chordObj.filter(chordObj => {
-        return chordObj.checked === true && chordObj.mainCat == "chordType";
-    })
-    var chordSymbol = chordKey[Math.floor((Math.random() * chordKey.length) + 0)].name + chordType[Math.floor((Math.random() * chordType.length) + 0)].name;
-    if (beat == meter) {
-        document.getElementById("chord-symbol").innerHTML = chordSymbol;
-        beat = 0;
-    }
-};
+  var meter = document.getElementById("meter-input").value;
+  var chordKey = chordObj.filter(chordObj => {
+    return chordObj.checked === true && chordObj.mainCat == "chordKey";
+  });
+  var chordType = chordObj.filter(chordObj => {
+    return chordObj.checked === true && chordObj.mainCat == "chordType";
+  });
+  var chordSymbol =
+    chordKey[Math.floor(Math.random() * chordKey.length + 0)].name +
+    chordType[Math.floor(Math.random() * chordType.length + 0)].name;
+  if (beat == meter) {
+    document.getElementById("chord-symbol").innerHTML = chordSymbol;
+    beat = 0;
+  }
+}
 function calcTempo() {
-    var beats = document.getElementById("bpm-input").value;
-    var minute = 60000;
-    tempo = minute / beats;
-    return tempo;
+  var beats = document.getElementById("bpm-input").value;
+  var minute = 60000;
+  tempo = minute / beats;
+  return tempo;
 }
 function metronome() {
-    disableControls(true)
-    timer = setInterval( param => {       
-        audio.play()
-        showChordSymbol()
-        beat++;
-    }, calcTempo())
-};
-function reset(){
-    clearInterval(timer)
-    disableControls(false)
+  disableControls(true);
+  timer = setInterval(param => {
+    audio.play();
+    showChordSymbol();
+    beat++;
+  }, calcTempo());
+}
+function reset() {
+  clearInterval(timer);
+  disableControls(false);
 }
 function disableControls(param) {
-    let controlIDs = ["start-button", "meter-input", "bpm-input"] // IDs of elements to disable while metronome is running
-    controlIDs.map(id => {document.getElementById(id).disabled = param;})
+  let controlIDs = ["start-button", "meter-input", "bpm-input"]; // IDs of elements to disable while metronome is running
+  controlIDs.map(id => {
+    document.getElementById(id).disabled = param;
+  });
 }
 function generateCheckboxes() {
-    chordObj.map(chordObj => {
-        let checkBox = `<input type="checkbox" id="${chordObj.name}" onclick="addSelectionToObject('${chordObj.name}')" ${chordObj.checked ? "checked" : ""}>
-                        <label for="${chordObj.name}">${chordObj.name}</label>`
-        document.getElementById(chordObj.mainCat + "-fieldset").innerHTML += checkBox
-    })
+  chordObj.map(chordObj => {
+    let checkBox = `<input type="checkbox" id="${
+      chordObj.name
+    }" onclick="addSelectionToObject('${chordObj.name}')" ${
+      chordObj.checked ? "checked" : ""
+    }>
+                        <label for="${chordObj.name}">${chordObj.name}</label>`;
+
+    document.getElementById(
+      chordObj.mainCat + "-fieldset"
+    ).innerHTML += checkBox;
+  });
 }
 function addSelectionToObject(id) {
-    chordObj.map(chord => {
-        if (chord.name == id) {
-            (chord.checked) ? chord.checked = false : chord.checked = true        
-        }
-    })
-};
+  chordObj.map(chord => {
+    if (chord.name == id) {
+      chord.checked ? (chord.checked = false) : (chord.checked = true);
+    }
+  });
+}
 function modal() {
-    var modal = document.getElementById('modal');
-    var btn = document.getElementById("settings");
-    var span = document.getElementsByClassName("close")[0];
-    modal.style.display = "block";
-    span.onclick = function () {
+  var modal = document.getElementById("modal");
+  var btn = document.getElementById("settings");
+  var span = document.getElementsByClassName("close")[0];
+  modal.style.display = "block";
+  span.onclick = function() {
     modal.style.display = "none";
+  };
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+  };
 }
